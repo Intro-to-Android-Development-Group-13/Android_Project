98b11cf0c5c9661f7e4c93e23f88d61926ecab0a
Alfonso <a.vieyra@hotmail.co.uk>
2023-11-11 02:11:50 -0800

    created api call using okhttp - alfonso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.test_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.test_app"
@@ -41,6 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9") // Use the latest version
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.test_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import android.util.Log
+import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getRecipes()
+
     }
+    private fun getRecipes() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val client = OkHttpClient()
+
+                val request = Request.Builder()
+                    .url("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ignorePantry=true&ranking=1")
+                    .get()
+                    .addHeader("X-RapidAPI-Key", "key")
+                    .addHeader("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+                    .build()
+
+                val response = client.newCall(request).execute()
+
+                withContext(Dispatchers.Main) {
+                    // Update UI or log response here
+                    Log.d("success", response.body?.string() ?: "No response")
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    // Handle error, update UI
+                    Log.e("error", e.message ?: "Error occurred")
+                }
+            }
+        }
+    }
+
 }
