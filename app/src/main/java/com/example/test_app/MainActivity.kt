62185972046635cabad786033eb097fdd2c7b62d
Alfonso <a.vieyra@hotmail.co.uk>
2023-11-11 04:55:04 -0800

    submit event listener, new activity added - Alfonso

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.test_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.RequestHeaders
 import com.codepath.asynchttpclient.RequestParams
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.*
+import android.content.Intent
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getRecipes()
+
+        // When user submits, api call to rapidapi will commence
+        val submit: Button = findViewById(R.id.button2)
+        submit.setOnClickListener {
+            getRecipes()
+        }
     }
 
     private fun getRecipes() {
@@ -23,20 +32,33 @@ class MainActivity : AppCompatActivity() {
         val params = RequestParams()
         params.put("ingredients", "cinnamon")
         params.put("number", "10")
+        params.put("rank", "10")
 
         val headers = RequestHeaders()
-        headers.put("X-RapidAPI-Key", "72e3afa377msh7ef35d064c63a73p1dc7b9jsn2241f09376c0")
+        headers.put("X-RapidAPI-Key", "key")
         headers.put("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
 
         client.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch", headers, params, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.d("success", "$json")
 
-                // Parse the JSON response to get the image URL
-                val imageUrl = json.jsonObject.getJSONArray("results").getJSONObject(0).getString("title")
+                // fetch json results - relevant keys: id, title and image
+                val resultsArray: JSONArray = json.jsonObject.getJSONArray("results")
+                val recipesArray: ArrayList<HashMap<String, String>> = ArrayList()
 
-
-                // Now you can use this URL to load the image, e.g., using an image loading library like Glide or Picasso
+                for ( i in 0 until resultsArray.length()) {
+                    val recipe = resultsArray.getJSONObject(i)
+                    recipesArray.add(hashMapOf(
+                        "id" to recipe.optString("id"),
+                        "title" to recipe.optString("title"),
+                        "image" to recipe.optString("image")
+                    ))
+                }
+                Log.d("parsed results", "$recipesArray")
+                // carry resultsArray to new activity
+                val intent = Intent(this@MainActivity, ResultsActivity::class.java)
+                intent.putExtra("recipes", recipesArray)
+                startActivity(intent)
             }
 
             override fun onFailure(statusCode: Int, headers: Headers?, response: String, throwable: Throwable?) {
